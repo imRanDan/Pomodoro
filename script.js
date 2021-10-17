57afc7d8cf2c302e87c9190db219a63ba3fecb83
Danyal <68073783+imRanDan@users.noreply.github.com>
2021-10-17 18:20:23 -0400

    figuring out the javascript for the timer before working on the css and html visual additions

--- script.js
+++ script.js
@@ -1,41 +1,18 @@
-var hours = 0;
-var mins = 0;
-var seconds = 0;
+workPeriod = 25
+restPeriod = 5
+seconds = 60
 
-$('#start').click(function(){
-    startTimer();
-});
+workMinutes = workPeriod - 1
+restMinutes = restPeriod - 1
 
-$('#stop').click(function(){
-    clearTimeout(timex);
-});
-
-$('#reset').click(function(){
-    hours = 0;  mins = 0;    seconds = 0;
-    $('#hours','#mins').html('00');
-    $('#seconds').html('00');
-});
-
-function startTimer(){
-    timex = setTimeout(function(){
-        seconds++;
-        if(seconds >59){seconds=0;mins++;
-            if(mins>59){
-                mins=0;hours++;
-                if(hours <10)
-    {$("#hours").text('0'+hours+':')} else
-    $("#hours").text(hours+':');
-            }
-
-    if(mins<10){
-        $("#mins").text('0'+mins+':');}
-        else $("#mins").text(mins+':');
-    }
-    if(seconds <10) {
-        $("#seconds").text('0'+seconds);} else {
-        $("#seconds").text(seconds);
-        }
-
-        startTimer();
-    },1000);
+let RemainingTime = () =>{
+    seconds = seconds - 1
+    if(seconds === 0){
+        workMinutes = workMinutes - 1
+        if(workMinutes === -1){
+            workMinutes = breakMinutes
 }
+        seconds = 60
+}
+}
+let timer = setInterval(RemainingTime , 1000)
